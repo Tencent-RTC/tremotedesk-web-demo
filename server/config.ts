3f3e7a285481145a9d8463ecd410a1b3be354f74
notedit <leeoxiang@gmail.com>
2023-01-06 18:41:08 +0800

    init repo

---
+++ server/config.ts
@@ -0,0 +1,7 @@
+
+
+
+export default {
+    appkey: 'xxxxx',
+    secret: 'xxxxxxxx',
+  };
---
+++ server/server.ts
@@ -0,0 +1,53 @@
+
+
+import express from "express";
+import http from "http";
+import cors from "cors";
+import jwt from 'jwt-simple';
+
+
+import config from "./config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/peer", async (req, res) => {
+    res.sendFile(__dirname + "/public/peer.html");
+});
+
+app.get('/', async (req, res) => {
+    res.redirect('/peer');
+});
+
+
+app.post("/token", async (req, res) => {
+
+    let roomid = req.body.roomid;
+    let userid = req.body.userid;
+    let role = req.body.role;
+    let expire = req.body.expire || 3600 * 24;
+
+    let data = {
+        appkey: config.appkey,
+        userid: userid,
+        role: role,
+        expire: expire
+    };
+
+    let token: string = jwt.encode(data, config.secret);
+
+    res.json({
+        code: 0,
+        data: { token: token },
+        message: 'ok',
+    });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
